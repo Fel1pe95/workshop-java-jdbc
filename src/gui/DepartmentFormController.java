e5dd8362da8595b76082f76b7655d6371e0a3629
Luis felipe <84816784+Fel1pe95@users.noreply.github.com>
2021-06-13 16:29:41 -0300

    saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,23 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 
 	private Department entity;
+	private DepartmentService service;
 	@FXML
 	private TextField id;
 	@FXML
@@ -26,17 +32,41 @@ public class DepartmentFormController implements Initializable {
 	private Label error;
 
 	@FXML
-	public void onSaveAction() {
-		System.out.println("onSaveAction");
+	public void onSaveAction(ActionEvent event) {
+		if(entity ==null) {
+			throw new IllegalStateException("Entity was null!");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service was null!");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+			
+		}catch(DbException e) {
+			Alerts.showAlert("Erro saving object!", null, e.getMessage(), AlertType.ERROR);
+		}
+		
+	}
+
+	private Department getFormData() {
+		Department obj = new Department();
+		obj.setId(Utils.tryParseToInt(id.getText()));
+		obj.setName(name.getText());
+		return obj;
 	}
 
 	@FXML
-	public void onCancelAction() {
-		System.out.println("onCancelAction");
+	public void onCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 
 	public void setDepartment(Department entity) {
 		this.entity = entity;
+	}
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 
 	@Override
