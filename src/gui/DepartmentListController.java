4d20f5b6a5c53dd3e143e81e1b401a4bd20500b3
Luis felipe <84816784+Fel1pe95@users.noreply.github.com>
2021-06-14 19:17:45 -0300

    padrao de projeto observer para atualizar tableview

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -25,7 +26,7 @@ import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
-public class DepartmentListController implements Initializable {
+public class DepartmentListController implements Initializable, DataChangeListener {
 	private DepartmentService service;
 	@FXML
 	private TableView<Department> tableViewDepartment;
@@ -78,6 +79,7 @@ public class DepartmentListController implements Initializable {
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
 			controller.setDepartmentService(new DepartmentService());
+			controller.subscribeDataChangeListener(this);
 			controller.updateFormData();
 			Stage dialogStage = new Stage(); 
 			dialogStage.setTitle("Enter department data");
@@ -90,6 +92,12 @@ public class DepartmentListController implements Initializable {
 		}catch(IOException e) {
 			Alerts.showAlert("IO exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
+		
+	}
+
+	@Override
+	public void onDataChanged() {
+		updateTableView();
 		
 	}
 
